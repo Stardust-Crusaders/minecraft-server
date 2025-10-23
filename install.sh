317bc5c4f61da968bb167155d29bcf308aa056e4
waxing-crescent <us4tiny4n@gmail.com>
2025-10-24 00:41:07 +0200

    install: fixes

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 mkdir ./server
 ln -s $(pwd)/mods $(pwd)/server
+ln -s $(pwd)/backups/neo-forge $(pwd)/server/world
 wget https://maven.neoforged.net/releases/net/neoforged/neoforge/21.1.213/neoforge-21.1.213-installer.jar -O ./server/installer.jar
 cd ./server
 java -jar ./installer.jar
